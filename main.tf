acf93c068a0e399bd32053beba4dbc10f803db0b
Valentin Reyes <valentin.reyes@oracle.com>
2019-04-16 18:00:55 -0500

    updated

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "aws_i1" {
        ami = "ami-0b1d4c689c7949a64" 
        instance_type = "t2.micro" 
 tags {
-   Name = "aws_vbrr"
+   Environmnet = "dev"
   }
 key_name = "vreyesr-pc"
 
