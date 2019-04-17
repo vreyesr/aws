ed9b4af5508086e86b6389dd05c7d241152629db
Valentin Reyes <valentin.reyes@oracle.com>
2019-04-17 16:25:06 -0500

    Update  EC2

--- aws_ec2_cli.py
+++ aws_ec2_cli.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 import boto3
+import json
 
 
 def aws_list_instances():
     ec2 = boto3.client('ec2')
     response=ec2
-    print response.describe_instances()
-
+    #print  json.dumps(response.describe_instances()['Reservations'])
+    for instances in response.describe_instances()['Reservations'][0]['Instances']:
+        for keys in instances.keys():
+            print "%s : %s" %( keys, instances[keys])
 
 aws_list_instances()
