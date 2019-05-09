9bf47fa34f945990709ef29b5a11cbfd156652d9
Valentin Reyes <valentin.reyes@gmail.com>
2019-05-09 15:13:37 -0500

    New file to update

--- ec2-vr2/manage_ec2.py
+++ ec2-vr2/manage_ec2.py
@@ -26,15 +26,39 @@ def get_rds_client():
     rds = boto3.client('rds')
     """:type : pyboto3.rds """
     return rds
-"""
-print dir(get_ec2_client())
-print type(get_ec2_client())
-print get_ec2_client()
-print get_ec2_client().describe_instances()
-print get_ec2_client().describe_instances()['Reservations'][0]['Instances']
-print type( get_ec2_client().describe_instances())
+
+j=get_ec2_client().describe_instances()
+i=get_ec2_client().describe_instances()['Reservations']
+ec2_l=[]
+for c in range(len(i)):
+    for d in i:
+        #ec2_l.append(d['Instances'][c]#['PublicDnsName'])
+        ec2_l.append(d['Instances'][c]['PublicDnsName'])
+        ec2_l.append(d['Instances'][c]['State']['Name'])
+        ec2_l.append(d['Instances'][c]['PublicIpAddress'])
+        ec2_l.append(d['Instances'][c]['PrivateIpAddress'])
+        ec2_l.append(d['Instances'][c]['InstanceId'])
+        ec2_l.append(d['Instances'][c]['ImageId'])
+        print ec2_l
+
 """
 
-print type(get_rds_client())
-print get_ec2_client().describe_instances()['Reservations'][0]['Instances'][0]['PublicDnsName']
-print get_rds_client().describe_db_instances()
+r=get_ec2_client().describe_regions()
+print r.get('Regions')
+#help (dict.iterkeys)
+#print type(r['Regions'])
+l=[]
+for c in r.iterkeys():    # dict
+    for i in r['Regions']: #list
+        #print type(i)      # dict
+        #print " {:39} : {} ".format(i.get('Endpoint'),i.get('RegionName'))
+        l.append(i['Endpoint'])
+        l.append(i['RegionName'])
+
+print l.sort()
+
+        #kl.append((i.get('Endpoint'),i.get('RegionName'))
+        #print i.get('Endpoint')
+        #print i.get('RegionName')
+        #print i['Endpoint'],i['RegionName']           # dict
+"""
