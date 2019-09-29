5ac5b6c9bf922a766378041a24d461015e031a1f
Valentin Reyes <valentin.reyes@gmail.com>
2019-09-29 00:06:30 -0500

    New file to update

--- ec2-vr2/aws_ec2_cli.py
+++ ec2-vr2/aws_ec2_cli.py
@@ -18,13 +18,22 @@ client = boto3.client('s3',
     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
 )
 
+ec2 = boto3.client('ec2')
 
+def stop_ec2():
+    ec2.stop_instances(InstanceIds=['i-07a71a7dfcbf2001c'])
+
+def start_ec2():
+    ec2.start_instances(InstanceIds=['i-07a71a7dfcbf2001c'])
 def aws_list_instances():
-    ec2 = boto3.client('ec2')
-    response=ec2
-    #print  json.dumps(response.describe_instances()['Reservations'])
-    for instances in response.describe_instances()['Reservations'][0]['Instances']:
-        for keys in instances.keys():
-            print "%s : %s" %( keys, instances[keys])
+    compute=ec2.describe_instances()
+
+#    print [(k, i[k]) for i in compute['Reservations'][0]['Instances'] for k in i.keys()]
+    print [i[k] for i in compute['Reservations'][0]['Instances'] for k in ['State','PublicIpAddress','PublicDnsName' ] if "PublicIpAddress" in i]
 
 aws_list_instances()
+#stop_ec2()
+#start_ec2()
+aws_list_instances()
+
+
